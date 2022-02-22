c73ca1a6c3f87d08e3c0b84d65bc53f7a7dc4deb
withers56 <william.k.withers@gmail.com>
2022-02-22 11:51:42 -0600

    Changed color style of page, added navbar, moved create button

--- movie-database.js
+++ movie-database.js
@@ -31,24 +31,24 @@ function appendMovies() {
         for (let movie of movies) {
             $('#movie-container').append(
                 `
-                <div class="card col-12 col-sm-5 col-md-3 m-1">
+                <div class="card col-12 col-sm-5 col-md-3 m-1 py-2">
                   <div class="card-body card-plot">
                     <h5 class="card-title title">${movie.title}</h5>
                     <p class="card-text">${movie.plot}</p>
                   </div>
-                  <ul class="list-group list-group-flush">
+                  <ul class="list-group list-group-flush card-list">
                     <li class="list-group-item">Year: ${movie.year}</li>
                     <li class="list-group-item">Rating: ${movie.rating}/5</li>
                     <li class="list-group-item">Director: ${movie.director}</li>
                   </ul>
                   <div class="card-body d-flex justify-content-between">
                   
-                    <button type="button" data-toggle="modal" data-target="#exampleModal" class="edit-btn btn btn-dark" value="${movie.id}">edit</button>
+                    <button type="button" data-toggle="modal" data-target="#exampleModal" class="edit-btn btn" value="${movie.id}">edit</button>
                     
                 
 
                     
-                    <button class="delete-btn btn btn-dark" value="${movie.id}">delete</button>
+                    <button class="delete-btn btn" value="${movie.id}">delete</button>
                   </div>
                 </div>
                 `
@@ -60,18 +60,21 @@ function appendMovies() {
 function createMovie(movieInfo) {
     fetch(url, movieInfo).then(function (result){
         console.log('post created successfully');
+        appendMovies()
     });
 }
 
 function updateMovie(id, updateOptions) {
     fetch(`${url}/${id}`, updateOptions).then(function (result){
         console.log('updated successfully');
+        appendMovies()
     });
 }
 
 function deleteMovie(id){
     fetch(`${url}/${id}`,{method: 'DELETE'}).then(function (result){
         console.log('deleted successfully');
+        appendMovies();
     });
 }
 
@@ -151,6 +154,8 @@ $(document).on('click', '.edit-btn', function (){
             body: JSON.stringify(updatedMovie),
         };
         updateMovie(id, updateOptions);
+
+        $('#exampleModal')
     });
 });
 
